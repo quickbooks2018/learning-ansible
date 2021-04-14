2208c20d8ce63fb5cf0c5c384369917cc6ced298
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2021-04-14 13:51:42 +0500

    Update ansible-master.sh

--- ansible/vpc-ansible-ec2-controller-+-managed-nodes/aws/modules/aws-ec2/user-data/ansible/ansible-master.sh
+++ ansible/vpc-ansible-ec2-controller-+-managed-nodes/aws/modules/aws-ec2/user-data/ansible/ansible-master.sh
@@ -7,7 +7,7 @@ yum install -y docker nc
 systemctl start docker
 systemctl enable docker
 
-docker run --name jenkins -u root -id -v "$PWD"/jenkins:/var/jenkins_home -p 80:8080 -p 50000:50000 --restart unless-stopped jenkins/jenkins:lts
+docker run --name jenkins -u root -id -v "$PWD"/jenkins:/var/jenkins_home -p 80:8080 -p 50000:50000 --restart unless-stopped quickbooks2018/jenkins-ansible-maven:latest
 
 while ! nc -vz 127.0.0.1 80;do echo "Waiting for port" && sleep 5;done
 
